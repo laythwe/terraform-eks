84765a89c8a6191ce07ea6d6575bf27817507ac6
MyatThwe <119537916+Laythwe@users.noreply.github.com>
2024-12-22 23:15:30 +0630

    third commit

--- variables.tf
+++ variables.tf
@@ -2,24 +2,6 @@ variable "region" {
   type = string
   description = "AWS Region"
 } 
-
-variable "access_key" {
-  type = string
-  sensitive = true
-  description = "Access key"
-}
-
-variable "secret_key" {
-  type = string
-  sensitive = true
-  description = "Secret key"
-}
-
-variable "cluster_name" {
-  type =  string
-  description = "Name of the Cluster"
-}
-
 variable "vpc_id" {
   type = string
   description = "ID of the vpc"
